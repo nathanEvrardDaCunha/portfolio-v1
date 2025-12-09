4be4ed16971c9512c2b29deb87ca171098304080
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-09 16:44:48 +0100

    chore: setup Prettier and prettier-plugin-tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 function App() {
-  // TODO: Use proper html semantic structure with <h1>, <h2>...
-  return (
-    <>
-      <h1 className="text-5xl text-gray-900 font-extrabold">Hello World</h1>
-    </>
-  );
+    // TODO: Use proper html semantic structure with <h1>, <h2>...
+    return (
+        <>
+            <h1 className="text-5xl font-extrabold text-gray-900">Hello World</h1>
+
+            <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+        </>
+    );
 }
 
 export default App;
