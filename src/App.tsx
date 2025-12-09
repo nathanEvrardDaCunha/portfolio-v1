90fc6361f42ba04dac21bc0cb284a018f18fe066
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-09 17:00:56 +0100

    chore: install and setup clsx and twMerge

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import TestButton from './components/TestButton';
+
 function App() {
     // TODO: Use proper html semantic structure with <h1>, <h2>...
     return (
         <>
             <h1 className="text-5xl font-extrabold text-gray-900">Hello World</h1>
 
-            <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+            <TestButton className="mt-8 ml-8">Click me</TestButton>
         </>
     );
 }
