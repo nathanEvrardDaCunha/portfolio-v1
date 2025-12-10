26e24b39a5fe13c5749cd847e96eec303a37cd5c
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-10 14:48:44 +0100

    feat: create draft for Home page work section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import HomeHero from './HomeHero';
+import HomeWork from './HomeWork';
 
 const Home = () => {
     // TODO: Use proper html semantic structure with <h1>, <h2>...
     // TODO: Make sure there is no language (ex: grammatical) errors/mistakes.
+    // TODO: Match the education description of each job with the education description on my portfolio
 
-    return <HomeHero />;
+    return (
+        <>
+            <HomeHero />
+
+            <HomeWork />
+        </>
+    );
 };
 
 export default Home;
---
+++ src/pages/HomeWork.tsx
@@ -0,0 +1,62 @@
+const HomeWork = () => {
+    // TODO: When the user click on the company where I work, redirect it (always in a new tab) to the official website of my previous employer.
+    // TODO: Match the work description of each job with the work description on my portfolio
+    return (
+        <section>
+            <h2>Work</h2>
+
+            <article>
+                {/* TODO: Replace alt proper image description. */}
+                {/* TODO: Replace src with proper image. */}
+                {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
+                <img src="https://picsum.photos/20" alt="Alt img" />
+                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
+                <p> - </p>
+                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
+                <h3>Company Name</h3>
+                <strong>Job Title (Type Of Contract - ex: Intern)</strong>
+                <ul>
+                    <li>Mission number 1 was about X.</li>
+                    <li>Mission number 1 was about X.</li>
+                    <li>Mission number 1 was about X.</li>
+                </ul>
+            </article>
+
+            <article>
+                {/* TODO: Replace alt proper image description. */}
+                {/* TODO: Replace src with proper image. */}
+                {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
+                <img src="https://picsum.photos/20" alt="Alt img" />
+                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
+                <p> - </p>
+                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
+                <h3>Company Name</h3>
+                <strong>Job Title (Type Of Contract - ex: Intern)</strong>
+                <ul>
+                    <li>Mission number 1 was about X.</li>
+                    <li>Mission number 1 was about X.</li>
+                    <li>Mission number 1 was about X.</li>
+                </ul>
+            </article>
+
+            <article>
+                {/* TODO: Replace alt proper image description. */}
+                {/* TODO: Replace src with proper image. */}
+                {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
+                <img src="https://picsum.photos/20" alt="Alt img" />
+                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
+                <p> - </p>
+                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
+                <h3>Company Name</h3>
+                <strong>Job Title (Type Of Contract - ex: Intern)</strong>
+                <ul>
+                    <li>Mission number 1 was about X.</li>
+                    <li>Mission number 1 was about X.</li>
+                    <li>Mission number 1 was about X.</li>
+                </ul>
+            </article>
+        </section>
+    );
+};
+
+export default HomeWork;
