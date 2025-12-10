1fc394447ddedbedf06fa3f2008bc313d3bd6ea2
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-10 17:34:02 +0100

    feat: implement basic visual style for HomeWork

--- src/pages/HomeWork.tsx
+++ src/pages/HomeWork.tsx
@@ -2,59 +2,115 @@ const HomeWork = () => {
     // TODO: When the user click on the company where I work, redirect it (always in a new tab) to the official website of my previous employer.
     // TODO: Match the work description of each job with the work description on my portfolio
     return (
-        <section>
-            <h2>Work</h2>
+        <section className="mx-auto max-w-3xl px-8 pb-16">
+            <h2 className="mb-8 text-2xl font-bold text-neutral-950 dark:text-neutral-50">Work</h2>
 
-            <article>
-                {/* TODO: Replace alt proper image description. */}
-                {/* TODO: Replace src with proper image. */}
-                {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
-                <img src="https://picsum.photos/20" alt="Alt img" />
-                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
-                <p> - </p>
-                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
-                <h3>Company Name</h3>
-                <strong>Job Title (Type Of Contract - ex: Intern)</strong>
-                <ul>
-                    <li>Mission number 1 was about X.</li>
-                    <li>Mission number 1 was about X.</li>
-                    <li>Mission number 1 was about X.</li>
-                </ul>
-            </article>
+            <div className="flex flex-col gap-8">
+                <article className="flex flex-row gap-8">
+                    {/* TODO: Replace alt proper image description. */}
+                    {/* TODO: Replace src with proper image. */}
+                    {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
+                    <img
+                        src="https://picsum.photos/46"
+                        alt="Alt img"
+                        className="aspect-square h-fit w-fit rounded-lg"
+                    />
 
-            <article>
-                {/* TODO: Replace alt proper image description. */}
-                {/* TODO: Replace src with proper image. */}
-                {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
-                <img src="https://picsum.photos/20" alt="Alt img" />
-                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
-                <p> - </p>
-                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
-                <h3>Company Name</h3>
-                <strong>Job Title (Type Of Contract - ex: Intern)</strong>
-                <ul>
-                    <li>Mission number 1 was about X.</li>
-                    <li>Mission number 1 was about X.</li>
-                    <li>Mission number 1 was about X.</li>
-                </ul>
-            </article>
+                    <div className="flex flex-col">
+                        <div className="flex flex-row gap-2">
+                            <time dateTime="YYYY-MM-DD" className="text-[12px] text-neutral-700 dark:text-neutral-300">
+                                YYYY-MM-DD
+                            </time>
+                            <p className="text-[12px] text-neutral-700">-</p>
+                            <time dateTime="YYYY-MM-DD" className="text-[12px] text-neutral-700 dark:text-neutral-300">
+                                YYYY-MM-DD
+                            </time>
+                        </div>
 
-            <article>
-                {/* TODO: Replace alt proper image description. */}
-                {/* TODO: Replace src with proper image. */}
-                {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
-                <img src="https://picsum.photos/20" alt="Alt img" />
-                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
-                <p> - </p>
-                <time dateTime="YYYY-MM-DD">YYYY-MM-DD</time>
-                <h3>Company Name</h3>
-                <strong>Job Title (Type Of Contract - ex: Intern)</strong>
-                <ul>
-                    <li>Mission number 1 was about X.</li>
-                    <li>Mission number 1 was about X.</li>
-                    <li>Mission number 1 was about X.</li>
-                </ul>
-            </article>
+                        <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">Company Name</h3>
+
+                        <p className="text-base text-neutral-700 dark:text-neutral-300">
+                            Job Title (Type Of Contract - ex: Intern)
+                        </p>
+
+                        <ul className="flex list-inside list-disc flex-col">
+                            <li>Mission number 1 was about X.</li>
+                            <li>Mission number 1 was about X.</li>
+                            <li>Mission number 1 was about X.</li>
+                        </ul>
+                    </div>
+                </article>
+
+                <article className="flex flex-row gap-8">
+                    {/* TODO: Replace alt proper image description. */}
+                    {/* TODO: Replace src with proper image. */}
+                    {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
+                    <img
+                        src="https://picsum.photos/46"
+                        alt="Alt img"
+                        className="aspect-square h-fit w-fit rounded-lg"
+                    />
+
+                    <div className="flex flex-col">
+                        <div className="flex flex-row gap-2">
+                            <time dateTime="YYYY-MM-DD" className="text-[12px] text-neutral-700 dark:text-neutral-300">
+                                YYYY-MM-DD
+                            </time>
+                            <p className="text-[12px] text-neutral-700">-</p>
+                            <time dateTime="YYYY-MM-DD" className="text-[12px] text-neutral-700 dark:text-neutral-300">
+                                YYYY-MM-DD
+                            </time>
+                        </div>
+
+                        <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">Company Name</h3>
+
+                        <p className="text-base text-neutral-700 dark:text-neutral-300">
+                            Job Title (Type Of Contract - ex: Intern)
+                        </p>
+
+                        <ul className="flex list-inside list-disc flex-col">
+                            <li>Mission number 1 was about X.</li>
+                            <li>Mission number 1 was about X.</li>
+                            <li>Mission number 1 was about X.</li>
+                        </ul>
+                    </div>
+                </article>
+
+                <article className="flex flex-row gap-8">
+                    {/* TODO: Replace alt proper image description. */}
+                    {/* TODO: Replace src with proper image. */}
+                    {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
+                    <img
+                        src="https://picsum.photos/46"
+                        alt="Alt img"
+                        className="aspect-square h-fit w-fit rounded-lg"
+                    />
+
+                    <div className="flex flex-col">
+                        <div className="flex flex-row gap-2">
+                            <time dateTime="YYYY-MM-DD" className="text-[12px] text-neutral-700 dark:text-neutral-300">
+                                YYYY-MM-DD
+                            </time>
+                            <p className="text-[12px] text-neutral-700">-</p>
+                            <time dateTime="YYYY-MM-DD" className="text-[12px] text-neutral-700 dark:text-neutral-300">
+                                YYYY-MM-DD
+                            </time>
+                        </div>
+
+                        <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">Company Name</h3>
+
+                        <p className="text-base text-neutral-700 dark:text-neutral-300">
+                            Job Title (Type Of Contract - ex: Intern)
+                        </p>
+
+                        <ul className="flex list-inside list-disc flex-col">
+                            <li>Mission number 1 was about X.</li>
+                            <li>Mission number 1 was about X.</li>
+                            <li>Mission number 1 was about X.</li>
+                        </ul>
+                    </div>
+                </article>
+            </div>
         </section>
     );
 };
