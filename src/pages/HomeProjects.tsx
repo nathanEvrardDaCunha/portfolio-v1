37f7969a3a5294d37df8fa29fc39524df6229176
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-18 14:13:37 +0100

    feat: rewrite Project content with proper texts

--- src/pages/HomeProjects.tsx
+++ src/pages/HomeProjects.tsx
@@ -10,7 +10,7 @@ const HomeProjects = () => {
             <h2 className="mb-8 text-2xl font-bold text-neutral-950 dark:text-neutral-50">Featured projects</h2>
 
             <div className="flex flex-row flex-wrap gap-4">
-                <article className="min-w-3xs flex-1 rounded-sm border border-neutral-300 p-8 shadow-md">
+                <article className="flex min-w-3xs flex-1 flex-col rounded-sm border border-neutral-300 p-8 shadow-md">
                     {/* TODO: Replace alt proper image description. */}
                     {/* TODO: Replace src with proper image. */}
                     {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
@@ -18,20 +18,22 @@ const HomeProjects = () => {
                     <img src="https://picsum.photos/300/100" alt="Alt img" className="mb-8 max-h-48 min-h-32 w-full" />
 
                     <div className="flex flex-col">
-                        <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">Project Name</h3>
+                        <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">
+                            Cris'Alie Showcase Site
+                        </h3>
 
                         <strong className="mb-6 text-base font-normal text-neutral-700 dark:text-neutral-300">
-                            Type of Project (personal, professional...)
+                            Volunteering
                         </strong>
 
                         <p className="mb-6 text-base font-medium text-neutral-950 dark:text-neutral-50">
-                            This is the short yet compelling description of my amazing project summarizing in less than
-                            4 sentences why it's increadible.
+                            Design, build and put into production from A to Z the showcase site of Cris'Alie, a Narbonne
+                            sales shop, to make them visible online.
                         </p>
 
                         <ul className="mb-6 flex flex-row flex-wrap gap-1">
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">JavaScript</p>
+                                <p className="text-[12px] font-medium text-neutral-950">React</p>
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
@@ -39,7 +41,7 @@ const HomeProjects = () => {
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">React</p>
+                                <p className="text-[12px] font-medium text-neutral-950">JavaScript</p>
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
@@ -47,20 +49,33 @@ const HomeProjects = () => {
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">JavaScript</p>
+                                <p className="text-[12px] font-medium text-neutral-950">HTML5</p>
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">TypeScript</p>
+                                <p className="text-[12px] font-medium text-neutral-950">CSS3</p>
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">React</p>
+                                <p className="text-[12px] font-medium text-neutral-950">Responsive Design</p>
+                            </li>
+
+                            {/* TODO: Also add SEO tag in project and Tech Stack for performances reasons (like Lighthouse) or is it too much ? */}
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">Accessibility</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">Git</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">GitHub</p>
                             </li>
                         </ul>
                     </div>
 
-                    <div className="flex flex-row gap-2">
+                    <div className="mt-auto flex flex-row gap-2">
                         {/* TODO: Add link to this project live demo if there's one. */}
                         <a
                             href="#"
@@ -81,7 +96,7 @@ const HomeProjects = () => {
                     </div>
                 </article>
 
-                <article className="min-w-3xs flex-1 rounded-sm border border-neutral-300 p-8 shadow-md">
+                <article className="flex min-w-3xs flex-1 flex-col rounded-sm border border-neutral-300 p-8 shadow-md">
                     {/* TODO: Replace alt proper image description. */}
                     {/* TODO: Replace src with proper image. */}
                     {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
@@ -89,20 +104,20 @@ const HomeProjects = () => {
                     <img src="https://picsum.photos/300/100" alt="Alt img" className="mb-8 max-h-48 min-h-32 w-full" />
 
                     <div className="flex flex-col">
-                        <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">Project Name</h3>
+                        <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">Online Task Manager</h3>
 
                         <strong className="mb-6 text-base font-normal text-neutral-700 dark:text-neutral-300">
-                            Type of Project (personal, professional...)
+                            Personal
                         </strong>
 
                         <p className="mb-6 text-base font-medium text-neutral-950 dark:text-neutral-50">
-                            This is the short yet compelling description of my amazing project summarizing in less than
-                            4 sentences why it's increadible.
+                            Develop and put online a Full Stack task manager, suitable for visually impaired people and
+                            with secure authentication.
                         </p>
 
                         <ul className="mb-6 flex flex-row flex-wrap gap-1">
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">JavaScript</p>
+                                <p className="text-[12px] font-medium text-neutral-950">React</p>
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
@@ -110,99 +125,60 @@ const HomeProjects = () => {
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">React</p>
-                            </li>
-
-                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">Tailwind CSS</p>
-                            </li>
-
-                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
                                 <p className="text-[12px] font-medium text-neutral-950">JavaScript</p>
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">TypeScript</p>
+                                <p className="text-[12px] font-medium text-neutral-950">SASS/SCSS</p>
                             </li>
 
                             <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">React</p>
+                                <p className="text-[12px] font-medium text-neutral-950">HTML5</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">CSS3</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">Responsive Design</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">Accessibility</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">Git</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">GitHub</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">Node.js</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">Express</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">API RESTful</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">PostgreSQL</p>
+                            </li>
+
+                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
+                                <p className="text-[12px] font-medium text-neutral-950">SQL</p>
                             </li>
                         </ul>
                     </div>
 
-                    <div className="flex flex-row gap-2">
-                        {/* TODO: Add link to this project live demo if there's one. */}
-                        <a
-                            href="#"
-                            className="flex flex-row items-center gap-2 rounded-sm bg-neutral-950 px-2 py-1 hover:bg-neutral-800"
-                        >
-                            <Globe strokeWidth={2} className="h-4 w-4 text-neutral-50" />
-                            <p className="text-[12px] font-medium text-neutral-50">Website</p>
-                        </a>
-
-                        {/* TODO: Add link to this project public Github repository if there's one. */}
-                        <a
-                            href="#"
-                            className="flex flex-row items-center gap-2 rounded-sm bg-neutral-950 px-2 py-1 hover:bg-neutral-800"
-                        >
-                            <UIGitHubIcon className="h-4 w-4 text-neutral-50" />
-                            <p className="text-[12px] font-medium text-neutral-50">GitHub</p>
-                        </a>
-                    </div>
-                </article>
-
-                <article className="min-w-3xs flex-1 rounded-sm border border-neutral-300 p-8 shadow-md">
-                    {/* TODO: Replace alt proper image description. */}
-                    {/* TODO: Replace src with proper image. */}
-                    {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
-                    {/* TODO: Display the image in full when clicked. */}
-                    <img src="https://picsum.photos/300/100" alt="Alt img" className="mb-8 max-h-48 min-h-32 w-full" />
-
-                    <div className="flex flex-col">
-                        <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">Project Name</h3>
-
-                        <strong className="mb-6 text-base font-normal text-neutral-700 dark:text-neutral-300">
-                            Type of Project (personal, professional...)
-                        </strong>
-
-                        <p className="mb-6 text-base font-medium text-neutral-950 dark:text-neutral-50">
-                            This is the short yet compelling description of my amazing project summarizing in less than
-                            4 sentences why it's increadible.
-                        </p>
-
-                        <ul className="mb-6 flex flex-row flex-wrap gap-1">
-                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">JavaScript</p>
-                            </li>
-
-                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">TypeScript</p>
-                            </li>
-
-                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">React</p>
-                            </li>
-
-                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">Tailwind CSS</p>
-                            </li>
-
-                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">JavaScript</p>
-                            </li>
-
-                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">TypeScript</p>
-                            </li>
-
-                            <li className="rounded-sm bg-neutral-200 px-2 py-1">
-                                <p className="text-[12px] font-medium text-neutral-950">React</p>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className="flex flex-row gap-2">
+                    <div className="mt-auto flex flex-row gap-2">
                         {/* TODO: Add link to this project live demo if there's one. */}
                         <a
                             href="#"
