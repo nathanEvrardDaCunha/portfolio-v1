832202a02bc66469b8302c773026906fe69f0913
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-19 14:23:23 +0100

    feat: rework Work UI to look like Timeline

--- src/pages/HomeWork.tsx
+++ src/pages/HomeWork.tsx
@@ -4,52 +4,65 @@ import adiosLogo from '../images/adios_logo.jpg';
 const HomeWork = () => {
     // TODO: When the user click on the company where I work, redirect it (always in a new tab) to the official website of my previous employer.
     // TODO: Match the work description of each job with the work description on my portfolio
+
+    // TODO: Standardize gap (ex: gap-4, gap-5...)
+    // TODO: Standardize rounded (ex: rounded-sm, rounded-lg...)
     return (
         <section className="mx-auto max-w-3xl px-8 pb-16">
             <h2 className="mb-8 text-2xl font-bold text-neutral-950 dark:text-neutral-50">Professional experiences</h2>
 
-            <div className="flex flex-col gap-8">
-                <article className="flex flex-row gap-8">
-                    {/* TODO: Replace alt proper image description. */}
-                    {/* TODO: Replace src with proper image. */}
-                    {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
-                    <img src={adiosLogo} alt="Alt img" className="aspect-square h-12 w-12 rounded-lg" />
+            <div className="relative flex flex-col gap-8">
+                <div className="absolute top-0 bottom-0 left-10 w-0.5 bg-neutral-300 dark:bg-neutral-600"></div>
 
-                    <div className="flex flex-col">
-                        <div className="flex flex-row gap-1">
-                            <time dateTime="2022-02" className="text-[12px] text-neutral-700 dark:text-neutral-300">
-                                February 2022
-                            </time>
-                            <p className="text-[12px] text-neutral-700">-</p>
-                            <time dateTime="2022-05" className="text-[12px] text-neutral-700 dark:text-neutral-300">
-                                May 2022
-                            </time>
+                <div className="flex flex-col gap-8 rounded-lg border border-neutral-300 bg-neutral-50 px-4 py-4 shadow-sm hover:bg-neutral-200">
+                    <article className="flex flex-row gap-5">
+                        {/* TODO: Replace alt proper image description. */}
+                        {/* TODO: Replace src with proper image. */}
+                        {/* TODO: Tweak image loading strategy and priority to enhance Lighthouse score. */}
+                        <img
+                            src={adiosLogo}
+                            alt="Alt img"
+                            className="z-10 aspect-square h-12 w-12 rounded-full border border-neutral-300 bg-neutral-50 object-cover hover:bg-neutral-200"
+                        />
+
+                        <div className="flex flex-col">
+                            <div className="flex flex-row gap-1">
+                                <time dateTime="2022-02" className="text-[12px] text-neutral-700 dark:text-neutral-300">
+                                    February 2022
+                                </time>
+                                <p className="text-[12px] text-neutral-700">-</p>
+                                <time dateTime="2022-05" className="text-[12px] text-neutral-700 dark:text-neutral-300">
+                                    May 2022
+                                </time>
+                            </div>
+
+                            <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">Institut ADIOS</h3>
+
+                            <p className="mb-4 text-base text-neutral-700 dark:text-neutral-300">
+                                Internship as Web Developer
+                            </p>
+
+                            <ul className="ml-4 flex list-outside list-disc flex-col">
+                                <li>
+                                    Design of an automated workflow (Zapier, JavaScript scripts) reducing the
+                                    operational burden of the secretariat by 30%.
+                                </li>
+
+                                <li>
+                                    Development of an interactive form with Tally, improving user experience and
+                                    completion rate.
+                                </li>
+
+                                <li>
+                                    Visual redesign of WordPress articles and strategic integration of CTA to optimize
+                                    conversion.
+                                </li>
+
+                                <li>Mentorship of an intern on the company’s automation practices and workflows.</li>
+                            </ul>
                         </div>
-
-                        <h3 className="text-lg font-bold text-neutral-950 dark:text-neutral-50">Institut ADIOS</h3>
-
-                        <p className="text-base text-neutral-700 dark:text-neutral-300">Internship as Web Developer</p>
-
-                        <ul className="ml-4 flex list-outside list-disc flex-col">
-                            <li>
-                                Design of an automated workflow (Zapier, JavaScript scripts) reducing the operational
-                                burden of the secretariat by 30%.
-                            </li>
-
-                            <li>
-                                Development of an interactive form with Tally, improving user experience and completion
-                                rate.
-                            </li>
-
-                            <li>
-                                Visual redesign of WordPress articles and strategic integration of CTA to optimize
-                                conversion.
-                            </li>
-
-                            <li>Mentorship of an intern on the company’s automation practices and workflows.</li>
-                        </ul>
-                    </div>
-                </article>
+                    </article>
+                </div>
             </div>
         </section>
     );
