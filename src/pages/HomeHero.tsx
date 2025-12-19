9c72865a4410fe4fe6aaceb99d970c52dcc5ad26
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-19 13:58:55 +0100

    feat: rewrite hero layout to be mobile responsive

--- src/pages/HomeHero.tsx
+++ src/pages/HomeHero.tsx
@@ -5,7 +5,7 @@ import UIGitHubIcon from '../components/icons/UIGitHubIcon';
 const HomeHero = () => {
     return (
         <section className="mx-auto max-w-3xl px-8 py-16">
-            <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-8 sm:flex-row-reverse">
                 {/* TODO: Add clean image about myself */}
                 {/* TODO: Replace alt proper image description. */}
                 {/* TODO: Replace src with proper image. */}
@@ -16,7 +16,7 @@ const HomeHero = () => {
                     <h1 className="text-2xl font-bold text-neutral-950 dark:text-neutral-50">Hi Nathan here.</h1>
 
                     <p className="text-base font-medium text-neutral-950 dark:text-neutral-50">
-                        23 years old frontend developper from France 🇫🇷
+                        22 years old frontend developper from France 🇫🇷
                     </p>
 
                     <p className="text-base font-medium text-neutral-950 dark:text-neutral-50">
@@ -26,7 +26,7 @@ const HomeHero = () => {
                         <strong className="font-black text-blue-700">React</strong>.
                     </p>
 
-                    <div className="flex flex-row items-center gap-8">
+                    <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
                         {/* TODO: Make this link look like a button. */}
                         {/* TODO: Redirect to a .pdf page where my resume reside. */}
                         {/* TODO: Create a resume without any job title but only the relevant informations. */}
@@ -34,22 +34,24 @@ const HomeHero = () => {
                         <a
                             href="#"
                             target="_blank"
-                            className="flex flex-row items-center gap-3 rounded-sm border border-neutral-300 bg-neutral-50 px-4 py-2 shadow-sm hover:bg-neutral-200"
+                            className="flex w-fit flex-row items-center gap-3 rounded-sm border border-neutral-300 bg-neutral-50 px-4 py-2 shadow-sm hover:bg-neutral-200"
                         >
                             <p className="text-sm font-medium text-neutral-950">Resume</p>
                             <FileDown className="h-4 w-4 text-neutral-950" />
                         </a>
 
-                        <a href="https://www.linkedin.com/in/nathan-evrard/" target="_blank">
-                            <UILinkedInIcon className="h-5 w-5 text-neutral-950 hover:text-neutral-700" />
-                        </a>
-                        <a href="https://github.com/nathanEvrardDaCunha" target="_blank">
-                            <UIGitHubIcon className="h-5 w-5 text-neutral-950 hover:text-neutral-700" />
-                        </a>
+                        <div className="flex flex-row gap-4">
+                            <a href="https://www.linkedin.com/in/nathan-evrard/" target="_blank">
+                                <UILinkedInIcon className="h-5 w-5 text-neutral-950 hover:text-neutral-700" />
+                            </a>
+                            <a href="https://github.com/nathanEvrardDaCunha" target="_blank">
+                                <UIGitHubIcon className="h-5 w-5 text-neutral-950 hover:text-neutral-700" />
+                            </a>
 
-                        <a href="mailto:nathan.evrarddacunha@proton.me" target="_blank">
-                            <Mail strokeWidth={2} className="h-5 w-5 text-neutral-950 hover:text-neutral-700" />
-                        </a>
+                            <a href="mailto:nathan.evrarddacunha@proton.me" target="_blank">
+                                <Mail strokeWidth={2} className="h-5 w-5 text-neutral-950 hover:text-neutral-700" />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
