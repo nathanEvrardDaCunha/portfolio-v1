3df1bab0decab7f96549f04d43d10f5189684ffb
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-22 17:16:37 +0100

    feat: create UIParagraph and UISmallParagraph

---
+++ src/components/ui/paragraphs/UIParagraph.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+interface UIParagraphProps extends React.HTMLAttributes<HTMLParagraphElement> {
+    children: React.ReactNode;
+}
+
+const UIParagraph = ({ children, className, ...props }: UIParagraphProps) => {
+    return (
+        <p
+            className={twMerge(clsx('text-base font-medium text-neutral-950 dark:text-neutral-50'), className)}
+            {...props}
+        >
+            {children}
+        </p>
+    );
+};
+
+export default UIParagraph;
