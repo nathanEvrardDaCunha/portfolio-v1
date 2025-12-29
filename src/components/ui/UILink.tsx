3b1e1f0497cbc306bccc82d7c6d0dbc4bf35920e
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-29 16:09:26 +0100

    feat: create UILink to standardize <a>

---
+++ src/components/ui/UILink.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+interface UILinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
+    children: React.ReactNode;
+    href: string;
+    target?: '_blank';
+}
+
+// TODO: Replace <a with UILink ?
+const UILink = ({ children, href, target = '_blank', className, ...props }: UILinkProps) => {
+    return (
+        <a href={href} target={target} className={twMerge(clsx(''), className)} {...props}>
+            {children}
+        </a>
+    );
+};
+
+export default UILink;
